6e9c7e04d18cae3a102f27df2f6e6425bd872d7d
Sarvagy Agarwal <sarvagy.agarwal@pune.corp.reflexisinc.com>
2018-08-29 17:35:53 +0530

    trying comments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Counter from './Counter' ;
 import { Provider } from 'react-redux' ;
 import { createStore } from 'redux' ;
 import * as constants from './constants' ;
-
+// visual studio comment
 const reducer = (state = constants.initialState , action) => {
     console.log(action) ;
     let newState ;
